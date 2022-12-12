8766c48827a4b14f900fdfa7c945b99c3b64b11e
KenjiMobutu <kenjimobutu@gmail.com>
2022-12-12 16:57:18 +0100

    build(model)Repartition template items

---
+++ model/Repartition_template_items.php
@@ -0,0 +1,29 @@
+<?php
+require_once "framework/Model.php";
+
+  class Repartition_template_items extends Model{
+    public ?int $weight;
+    public ?int $user;
+    public ?int $repartition_templates;
+  }
+
+  public function __construct(?int $weight,String $user,int $repartition_templates){
+    $this->weight = $weight;
+    $this->user = $user;
+    $this->repartition_templates= $repartition_templates;
+  }
+  public function get_weight(): int {
+    return $this->weight;
+  }
+
+  public function get_weight_by_user($user): int {
+    $query = self::execute("SELECT weight FROM  `repartition_templates` where tricount=:tricount", array("tricount"=>$tricount));
+          $data = $query->fetch();//un seul resultat max
+          if ($query->rowCount() == 0){
+              return null;
+          } else
+            return ($data["weight"]);
+  }
+
+
+?>
