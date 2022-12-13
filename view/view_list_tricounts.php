6a050242b7e4b3f1ae81628eb4c69833aacbaccd
KenjiMobutu <kenjimobutu@gmail.com>
2022-12-13 11:10:30 +0100

    build(view) list_tricount

--- view/view_list_tricounts.php
+++ view/view_list_tricounts.php
@@ -7,47 +7,82 @@
         <title>
             <?= $user->getFullName() ?>'s Tricount!
         </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="css/style.css" rel="stylesheet" type="text/css" />
-        <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&family=Sen:wght@400;700;800&display=swap"
-            rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <link rel="icon" type="image/png" href="assets/img/favicon.png" sizes="32x32">
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/img/icon/192x192.png">
+        <link rel="stylesheet" href="css/style.css">
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
 
     <body>
-        <?php include 'menu.html' ?>
-        <div class="prf-body">
-            <div class="title">
-                <?= $user->getFullName() ?>'s Tricount!
+        <!-- loader -->
+        <div id="loader">
+            <img src="css/img/loading-icon.png" alt="icon" class="loading-icon">
+        </div>
+        <!-- * loader -->
+
+        <div class="appHeader">
+            <div class="left">
+                <a href="#" class="headerButton goBack">
+                    <ion-icon name="chevron-back-outline"></ion-icon>
+                </a>
             </div>
-            <div class="profile">
-                <div class="name">
-                    <h1>PROFILEEEEEEEEEEEEEE</h1>
-                    <div>
-                        <p>Hey
-                            <?php echo $user->getFullName(); ?> !
-                        </p>
-                    </div>
-                    <p>I know your email address is <span style="color:#e34646">
-                            <?php echo $user->getMail(); ?>
-                        </span>.</p>
-                    <p>What can i do for you?</p>
-                    <!-- LE ID S'AFFICHE QUE SI ON LOGOUT ET RE LOGIN pas besoin de lui la mais pour savoir-->
-                </div>
+            <div class="pageTitle">
+                Your tricount
             </div>
-            <div class="profile-buttons">
-                <ul>
-                    <li><a class="create" href="">Create tricount</a></li>
-                    <li><a class="edit" href="">Edit profile</a></li>
-                    <li><a class="change" href="">Change password</a></li>
-                    <li><a class="logout" href="user/logout">Logout</a></li>
-                </ul>
+            <div class="right">
+                <a href="tricount/add" class="headerButton goBack" >
+                    <ion-icon name="add-outline"></ion-icon>
+                </a>
             </div>
         </div>
+        <div id="listTricount">
+                <div class="section mt-2">
+                    <!-- tricount block -->
+                <?php foreach ($tricounts_list as $tl):  ?>
+                    <div class="card-block mb-2">
+                        <div class="card-main">
+                            <div class="card-button dropdown">
+                                <button type="button" class="btn btn-link btn-icon" data-bs-toggle="dropdown">
+                                    <ion-icon name="ellipsis-horizontal"></ion-icon>
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-end">
+                                    <a class="dropdown-item" href="#">
+                                        <ion-icon name="pencil-outline"></ion-icon>Edit
+                                    </a>
+                                    <a class="dropdown-item" href="#">
+                                        <ion-icon name="close-outline"></ion-icon>Remove
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="balance">
+                                <span class="label"><?=$tl->title ?></span>
+                                <h1 class="title"><?= $tl->description  == null ? "No description" : $tl->description ?></h1>
+                            </div>
+                            <div class="in">
+                                <div class="card-number">
+                                    <span class="label">With ... friends </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+        </div>
+    <!-- * tricount block -->
 
-
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+        <!-- Splide -->
+        <script src="css/src/splide/splide.min.js"></script>
+        <!-- Base Js File -->
+        <script src="css/src/js/base.js"></script>
     </body>
 
 </html>
