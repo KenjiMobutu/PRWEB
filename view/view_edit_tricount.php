19fc09be8a8f5e2e8cc0ca06e0521c4ddb6aad8d
KenjiMobutu <kenjimobutu@gmail.com>
2023-01-28 23:02:49 +0100

    feat(view delete_tricount)delete confirm view added

--- view/view_edit_tricount.php
+++ view/view_edit_tricount.php
@@ -39,45 +39,46 @@
         </div>
 
     <!-- * tricount block -->
-        <div class="edit-settingsTitle">
-            <h1>Settings</h1>
+        <div class="edit-tricount">
+            <div class="edit-settingsTitle">
+                <h1>Settings</h1>
+            </div>
+            <div class="edit-settingsInput">
+                <h2>Title :</h2>
+                <input name='title' value='<?= $tricount->get_title() ?>'>
+                <h2>Description (optional) :</h2>
+                <input name='description' value='<?= $tricount->get_description() == null ? "no Description" : $tricount->get_description()?>'>
+            </div>
+            <div class="edit-settingsTitle">
+                <h1>Subscriptions</h1>
+                <?php foreach ($sub as $s):  ?>
+                    <li>
+                        <input name='name' value='<?= $s->getFullName() == null ? "no Description" : $s->getFullName() ?>'>
+                    </li>
+                <?php endforeach; ?>
+                <form id="addSubscriber" action="participation/add/<?= $tricount->get_id() ?>" method="post">
+                    <div class="edit-selectSub">
+                        <select class="selectSub" name="names" id="names">
+                        <option value="">--Add a new subscriber--</option>
+                            <?php foreach ($users as $u):  ?>
+                                <option name="subName" id="subName" value='<?= $u->getUserId()?>'><?= $u->getFullName()?></option>
+                            <?php endforeach; ?>
+                        </select>
+                        <button>add</button>
+                    </div>
+                </form>
+            </div>
+            <div class="button-manage-repartition-template">
+                <form action="https://www.w3docs.com/">
+                    <button class="delete-tricount" type="submit">Manage template</button>
+                </form>
+            </div>
+            <div class="button-delete-tricount">
+                <form action="tricount/delete/<?= $tricount->get_id()?>" method="POST" >
+                    <button class="delete-tricount" type="submit">Delete this tricount</button>
+                </form>
+            </div>
         </div>
-        <div class="edit-settingsInput">
-            <h2>Title :</h2>
-            <input name='title' value='<?= $tricount->get_title() ?>'>
-            <h2>Description (optional) :</h2>
-            <input name='description' value='<?= $tricount->get_description() == null ? "no Description" : $tricount->get_description()?>'>
-        </div>
-        <div class="edit-settingsTitle">
-            <h1>Subscriptions</h1>
-            <?php foreach ($sub as $s):  ?>
-                <li>
-                    <input name='name' value='<?= $s->getFullName() == null ? "no Description" : $s->getFullName() ?>'>
-                </li>
-            <?php endforeach; ?>
-            <form id="addSubscriber" action="participation/add/<?= $tricount->get_id() ?>" method="post">
-                <div class="edit-selectSub">
-                    <select class="selectSub" name="names" id="names">
-                    <option value="">--Add a new subscriber--</option>
-                        <?php foreach ($users as $u):  ?>
-                            <option name="subName" id="subName" value='<?= $u->getUserId()?>'><?= $u->getFullName()?></option>
-                        <?php endforeach; ?>
-                    </select>
-                    <button>add</button>
-                </div>
-            </form>
-        </div>
-        <div class="button-manage-repartition-template">
-            <form action="https://www.w3docs.com/">
-                <button type="submit">Click me</button>
-            </form>
-        </div>
-        <div class="button-delete-tricount">
-            <form action="tricount/delete/<?= $tricount->get_id()?>" method="POST" >
-                <button class="delete-tricount" type="submit">Delete this tricount</button>
-            </form>
-        </div>
-
         <!-- JavaScript Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
