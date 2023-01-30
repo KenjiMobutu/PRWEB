5d0d9baabe4cb52366db1d563453977b21fa7af4
KenjiMobutu <kenjimobutu@gmail.com>
2023-01-30 16:41:01 +0100

    refactor

--- view/view_expenses.php
+++ view/view_expenses.php
@@ -19,7 +19,6 @@
         padding: 0;
         }
 
-<<<<<<< HEAD
         .data-card {
         width: 300px;
         height: 150px;
@@ -30,99 +29,6 @@
         margin: 20px;
         overflow: hidden;
         transition: 0.3s;
-=======
-                .view-balance-button:hover {
-                    /* Change button elevation on hover */
-                    transform: translateY(-2px);
-                    box-shadow: 2px 4px 8px rgba(0, 0, 0, 0.2);
-                }
-
-                .view-balance-button:active {
-                    /* Change button elevation when active */
-                    transform: translateY(2px);
-                    box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.2);
-                }
-
-
-
-                .container {
-                width: 90%;
-                margin: 0 auto;
-            }
-
-            .amount{
-                margin-top: -12px;
-                float : right;
-            }
-
-            .date{
-                margin-top: -18px;
-                float:right;
-            }
-
-            .data-list {
-                display: flex;
-                flex-wrap: wrap;
-                list-style: none;
-                padding: 0;
-            }
-
-            .data-item {
-                flex: 1;
-                margin: 10px;
-                border: 1px solid #ccc;
-                border-radius: 10px;
-                padding: 20px;
-            }
-
-            .data-card {
-                margin-bottom: 20px;
-                border-bottom: 2px solid grey;
-
-            }
-
-            .title {
-                font-size: 20px;
-                font-weight: bold;
-                margin-bottom: 10px;
-            }
-
-            .amount {
-                font-size: 18px;
-                margin-bottom: 10px;
-            }
-
-            .initiator {
-                font-size: 16px;
-                margin-bottom: 10px;
-            }
-
-            .date {
-                font-size: 14px;
-            }
-
-            .exp{
-                float:right;
-                margin-right:150px;
-                background-color:grey;
-                padding-left:15%;
-            }
-
-            .mytot{
-                margin-top:-40px;
-                padding-right:15%;
-                float:left;
-                margin-left:150px;
-                background-color:grey;
-            }
-
-
-        .add-btn {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            margin-left:20%;
->>>>>>> origin/feat_Kenji
         }
 
         .data-card:hover {
@@ -212,7 +118,6 @@
         }
 
         .add-button {
-<<<<<<< HEAD
         background-color: #00BFA5;
         border-radius: 50%;
         width: 60px;
@@ -221,15 +126,6 @@
         display: flex;
         justify-content: center;
         align-items: center
-=======
-            background-color: blue;
-            color: white;
-            border-radius: 50%;
-            width: 40px;
-            height: 40px;
-            font-size: 20px;
-            cursor: pointer;
->>>>>>> origin/feat_Kenji
         }
 
         p{
@@ -240,15 +136,10 @@
 
 
     @media screen and (min-width: 320px) and (max-width: 480px) {
-<<<<<<< HEAD
-        .view-balance-button {         
+        .view-balance-button {
             display: block;
             margin: 0 auto;
             text-align: center;
-=======
-        .view-balance-button {
-
->>>>>>> origin/feat_Kenji
             background-color: green;
             color: white;
             padding: 10px 20px;
@@ -271,24 +162,14 @@
 <body>
     <?php include 'menu.html' ?>
     <div class="cont">
-<<<<<<< HEAD
-        <div class="view_expenses">     
+        <div class="view_expenses">
             <button class="edit-btn">
                 <a href="https://www.example.com/edit" style="text-decoration: none; color: black;">Edit</a>
             </button>
-                <p><?php echo $tricount->get_title();?> > Expenses</p>  
-            
-                      
-                    <!-- <a href="/prwb_2223_c03/Operation/balance/<?php echo $tricount->get_id()?>">    
-=======
-        <div class="view_expenses">
-        <button class="edit-btn">
-            <a href="tricount/edit/<?= $tricount->get_id()?>" style="text-decoration: none; color: black;">Edit</a>
-        </button>
+                <p><?php echo $tricount->get_title();?> > Expenses</p>
 
-                    <p><?php echo $tricount->get_title();?> > Expenses</p>
-                    <a href="/prwb_2223_c03/Operation/balance/<?php echo $tricount->get_id()?>">
->>>>>>> origin/feat_Kenji
+
+                    <!-- <a href="/prwb_2223_c03/Operation/balance/<?php echo $tricount->get_id()?>">
                         <button class="view-balance-button">
                             <i class="fas fa-dollar-sign"></i>View Balance
                         </button>
@@ -322,21 +203,20 @@
                                                 echo '</li>';
                                                 echo '<div class="totals">
                                                 <div class="mytot">
-                                                    <p>MY TOTAL <br> '.round($mytot["0"]).' €</p>                  
+                                                    <p>MY TOTAL <br> '.round($mytot["0"]).' €</p>
                                                 </div>
-<<<<<<< HEAD
                                                 <div class="add-btn">
-                                                    <a href="/prwb_2223_c03/Operation/add/'.$tricount->get_id().'">   
+                                                    <a href="/prwb_2223_c03/Operation/add/'.$tricount->get_id().'">
                                                         <button class="add-button">+
                                                         </button>
                                                     </a>
                                                 </div>
                                                 <div class="exp">
-                                                    <p>TOTAL EXPENSES <br> '.round($totalExp["0"]).' €</p>                    
+                                                    <p>TOTAL EXPENSES <br> '.round($totalExp["0"]).' €</p>
                                                 </div>
                                             </div>';
-                                        } 
-                                
+                                        }
+
                                     if(empty($amount) && ($participants > 1)) {
                                         echo "<h1 style='text-align:center;'>this is empty :(</h1>";
                                             echo '<a href="/prwb_2223_c03/Operation/add/'.$tricount->get_id().'">';
@@ -346,7 +226,7 @@
                                                 echo '</a>';
                                                 echo '<div class="totals">
                                                 <div class="mytot">
-                                                <p>MY TOTAL <br> 0 €</p>                  
+                                                <p>MY TOTAL <br> 0 €</p>
                                               </div>
                                                 <div class="exp">';
                                                 echo '<p>TOTAL EXPENSES <br> 0 €</p>';
@@ -361,37 +241,25 @@
                                                 echo '</a>';
                                                 echo '<div class="totals">
                                                 <div class="mytot">
-                                                    <p>MY TOTAL <br> 0 €</p>                  
+                                                    <p>MY TOTAL <br> 0 €</p>
                                                 </div>
                                                 <div class="add-btn">
-                                                    <a href="/prwb_2223_c03/Operation/add/'.$tricount->get_id().'">    
+                                                    <a href="/prwb_2223_c03/Operation/add/'.$tricount->get_id().'">
                                                         <button class="add-button">+
                                                         </button>
                                                     </a>
                                                 </div>
                                                 <div class="exp">
-                                                echo <p>TOTAL EXPENSES <br> 0 €</p>;              
+                                                echo <p>TOTAL EXPENSES <br> 0 €</p>;
                                             </div>
                                             </div>';
                                         }endforeach;
                                 }
                                 ?>
-=======
-                                            </a>';
-                                            endforeach;
-                                        echo '</li>';
-                                    endforeach;
-                                else:
-                                    echo '<p>dommage</p>';
-                                endif;?>
->>>>>>> origin/feat_Kenji
                             </ul>
 
                         </div>
                         </ul>
-<<<<<<< HEAD
-        </div>
-=======
             </div>
             <div class=totals>
                 <div class="exp">
@@ -410,7 +278,6 @@
             </div>
 
             </div>
->>>>>>> origin/feat_Kenji
     </div>
 
 
